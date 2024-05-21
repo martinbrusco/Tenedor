5fa16c46c98c1199393c0bab904e81de7f630c0b
navarro-n <irenepinillos.n@gmail.com>
2024-05-21 13:04:56 +0200

    funcion tenedor y cuchillo

--- printCubierto.js
+++ printCubierto.js
@@ -1,27 +1,35 @@
+const input = require("prompt-sync")()
+var cubierto = prompt("Elige cubierto: ");
 
-Console.log("l  l  l");
-Console.log("l  l  l");
-Console.log("l  l  l");
-Console.log("l  l  l");
-Console.log("l  l  l");
-Console.log(" l l l ");
-Console.log("   l   ");
-Console.log("   l   ");
-Console.log("   l   ");
-Console.log("   l   ");
-Console.log("   l   ");
+if (cubierto === "tenedor") {
+    console.log(Tenedor());
+} if (cubierto === "cuchillo") {
+    console.log(Cuchillo());
+} else {
+    alert("Solo se puede elegir tenedor o cuchillo")
+}
 
-//CUCHILLO
-Console.log("   l   ");
-Console.log(" l l   ");
-Console.log("l  l   ");
-Console.log("l  l   ");
-Console.log("l  l   ");
-Console.log("l  l   ");
-Console.log(" l l   ");
-Console.log("   l   ");
-Console.log("   l   ");
-Console.log("   l   ");
-Console.log("   l   ");
-Console.log("   l   ");
+function Tenedor() {
+    console.log("Tenedor: ")
+    Console.log("l  l  l");
+    Console.log("l  l  l");
+    Console.log(" l l l ");
+    Console.log("   l   ");
+    Console.log("   l   ");
+    Console.log("   l   ");
+}
 
+
+function Cuchillo() {
+    console.log("Cuchillo: ")
+    Console.log("   l   ");
+    Console.log(" l l   ");
+    Console.log("l  l   ");
+    Console.log("l  l   ");
+    Console.log(" l l   ");
+    Console.log("   l   ");
+    Console.log("   l   ");
+    Console.log("   l   ");
+ 
+    
+}
